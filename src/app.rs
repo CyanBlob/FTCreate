8a6c3d2eaa66e0f4bc4d5c75450b7a18b3616955
Andrew Thomas <andrewthomas255@duck.com>
2023-12-18 07:58:06 -0600

    Revert "WIP Center Stage scoring"
    This reverts commit b5ff6620c5a6d35cefe2ba6638e4f489344bf031.

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,6 @@
 use crate::app::generators::generator::Generator;
 
 pub mod generators;
-pub mod scoring;
 
 use generators::motors::dc_motor::DcMotor;
 use generators::servos::rev_servo::RevServo;
@@ -15,15 +14,11 @@ use std::io::Write;
 use std::ops::Deref;
 use std::path::PathBuf;
 use tokio::sync::{mpsc, mpsc::unbounded_channel};
-use image::{DynamicImage, GenericImageView};
-use eframe::egui;
-use egui_extras::RetainedImage;
 
 #[cfg(not(target_arch = "wasm32"))]
 use tokio;
 #[cfg(not(target_arch = "wasm32"))]
 use tokio::runtime::Runtime;
-use crate::app::scoring::mosaic::Mosaic;
 
 pub mod syntax_highlighting;
 
@@ -54,16 +49,6 @@ pub struct TemplateApp {
     #[serde(skip)]
     #[cfg(not(target_arch = "wasm32"))]
     tokio_runtime: Runtime,
-    #[serde(skip)]
-    mosaic: Mosaic,
-    #[serde(skip)]
-    texture: Option<(egui::Vec2, egui::TextureId)>,
-    #[serde(skip)]
-    color_images: [RetainedImage; 5],
-    mosaic_auton: bool,
-    mosaic_spike: u8,
-    mosaic_auton_score: u32,
-    mosaic_has_prop: bool,
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
@@ -103,19 +88,6 @@ impl Default for TemplateApp {
             last_upload_update: UploadStatus::DISCONNECTED,
             #[cfg(not(target_arch = "wasm32"))]
             tokio_runtime: Runtime::new().unwrap(),
-            mosaic: Mosaic::default(),
-            texture: None,
-            color_images: [
-                RetainedImage::from_image_bytes("hex", include_bytes!("../assets/icon-256.png")).unwrap(),
-                RetainedImage::from_image_bytes("hex", include_bytes!("../assets/icon-256.png")).unwrap(),
-                RetainedImage::from_image_bytes("hex", include_bytes!("../assets/icon-256.png")).unwrap(),
-                RetainedImage::from_image_bytes("hex", include_bytes!("../assets/icon-256.png")).unwrap(),
-                RetainedImage::from_image_bytes("hex", include_bytes!("../assets/hex.png")).unwrap(),
-            ],
-            mosaic_auton: false,
-            mosaic_spike: 0,
-            mosaic_auton_score: 0,
-            mosaic_has_prop: false,
         }
     }
 }
@@ -226,81 +198,12 @@ impl TemplateApp {
 
         self.code = new_code.to_string();
     }
-
-    fn add_mosaic_window(&mut self, ctx: &egui::Context, ui: &mut egui::Ui) {
-        ui.label("Auton settings: ");
-        ui.horizontal(|ui| {
-            ui.add(
-                egui::Slider::new(&mut self.mosaic_spike, 1..=3)
-                    .text("Spike position")
-                    .step_by(1.0)
-                    .max_decimals(0),
-            );
-
-            ui.checkbox(&mut self.mosaic_auton, "In Auton");
-            ui.checkbox(&mut self.mosaic_has_prop, "Has prop");
-        });
-
-        ui.horizontal(|ui| {
-            if ui.button("Clear auton").clicked() {
-                self.mosaic.clear_auton();
-            }
-            if ui.button("Clear teleop").clicked() {
-                self.mosaic.clear_teleop();
-            }
-            if ui.button("Reset").clicked() {
-                self.mosaic.clear();
-            }
-        });
-
-        ui.horizontal(|ui| {
-            if ui.button("Copy auton to teleop").clicked() {
-                self.mosaic.copy_auton_to_teleop();
-            }
-            if ui.button("Copy teleop to auton").clicked() {
-                self.mosaic.copy_teleop_to_auton();
-            }
-        });
-
-        ui.label(format!("Current score: {} (Auton: {}, teleop: {})", self.mosaic.get_score(), self.mosaic.get_auton_score(), self.mosaic.get_teleop_score()));
-        ui.vertical(|ui| {
-            for y in (0..self.mosaic.max_height).rev() {
-                ui.horizontal(|ui| {
-                    let pixel_count = match y % 2 {
-                        0 => {
-                            self.mosaic.even_row_count
-                        }
-                        1 => {
-                            self.mosaic.odd_row_count
-                        }
-                        _ => { 0 } // math broke
-                    };
-                    if y % 2 == 0 {
-                        ui.add_space(32.0);
-                    }
-                    for x in 0..pixel_count {
-                        match self.mosaic_auton {
-                            true => {
-                                if ui.add(egui::ImageButton::new(self.color_images[self.mosaic.auton_pixels[x][y].color as usize].texture_id(ctx), [64.0, 64.0]).frame(false)).clicked() {
-                                    self.mosaic.auton_pixels[x][y].inc_color();
-                                }
-                            }
-                            false => {
-                                if ui.add(egui::ImageButton::new(self.color_images[self.mosaic.pixels[x][y].color as usize].texture_id(ctx), [64.0, 64.0]).frame(false)).clicked() {
-                                    self.mosaic.pixels[x][y].inc_color();
-                                }
-                            }
-                        }
-                    }
-                });
-            }
-        });
-    }
 }
 
 impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+
         // periodically update the UI without user interaction so build status can update
         match self.last_upload_update {
             UploadStatus::DISCONNECTED | UploadStatus::UPLOAD_FAILED | UploadStatus::CONNECT_FAILED | UploadStatus::BUILD_FAILED => {
@@ -452,11 +355,8 @@ impl eframe::App for TemplateApp {
 
             self.generate_code();
         });
-
-        /*egui::Window::new("Mosaic scoring").show(ctx, |ui| {
-            self.add_mosaic_window(ctx, ui);
-        });*/
     }
+
     /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
--- src/app/scoring/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod mosaic;
-pub mod pixel;
